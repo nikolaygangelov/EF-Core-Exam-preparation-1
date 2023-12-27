042b26ee0b37493b5a785b4555132fc9f454eeac
Nikolay Angelov <108483720+nikolaygangelov@users.noreply.github.com>
2023-12-27 17:09:59 +0200

    Update Deserializer.cs

--- EF Core Exam Preparation 1/Invoices/DataProcessor/Deserializer.cs
+++ EF Core Exam Preparation 1/Invoices/DataProcessor/Deserializer.cs
@@ -25,37 +25,49 @@
 
         public static string ImportClients(InvoicesContext context, string xmlString)
         {
+            //using Data Transfer Object Class to map it with Clients
             var serializer = new XmlSerializer(typeof(ImportClientsDTO[]), new XmlRootAttribute("Clients"));
+
+            //Deserialize method needs TextReader object to convert/map 
             using StringReader inputReader = new StringReader(xmlString);
             var clientsArrayDTOs = (ImportClientsDTO[])serializer.Deserialize(inputReader);
 
+            //using StringBuilder to gather all info in one string
             StringBuilder sb = new StringBuilder();
+
+            //creating List where all valid clients can be kept
             List<Client> clientsXML = new List<Client>();
 
             foreach (ImportClientsDTO client in clientsArrayDTOs)
             {
-                Client clientToAdd = new Client
-                {
-                    Name = client.Name,
-                    NumberVat = client.NumberVat,
-                };
-
+                //validating info for client from data
                 if (!IsValid(client))
                 {
                     sb.AppendLine(ErrorMessage);
                     continue;
                 }
 
+                //creating a valid client
+                Client clientToAdd = new Client
+                {
+                    // using identical properties in order to map successfully
+                    Name = client.Name,
+                    NumberVat = client.NumberVat,
+                };
+                
                 foreach (var address in client.Addresses)
                 {
+                    //validating info for address from data
                     if (!IsValid(address))
                     {
                         sb.AppendLine(ErrorMessage);
                         continue;
                     }
 
+                    //adding valid address
                     clientToAdd.Addresses.Add(new Address()
                     {
+                        // using identical properties in order to map successfully
                         City = address.City,
                         Country = address.Country,
                         PostCode = address.PostCode,
@@ -70,36 +82,44 @@
 
             context.Clients.AddRange(clientsXML);
 
+            //actually importing info from data
             context.SaveChanges();
 
+            //using TrimEnd() to get rid of white spaces
             return sb.ToString().TrimEnd();
         }
 
 
         public static string ImportInvoices(InvoicesContext context, string jsonString)
         {
+            //using Data Transfer Object Class to map it with Invoices
             var invoicesArray = JsonConvert.DeserializeObject<ImportInvoicesDTO[]>(jsonString);
 
+            //using StringBuilder to gather all info in one string
             StringBuilder sb = new StringBuilder();
+
+            //creating List where all valid invoices can be kept
             List<Invoice> invoices = new List<Invoice>();
 
             foreach (ImportInvoicesDTO invoice in invoicesArray)
             {
-
+                //validating info for invoice from data
                 if (!IsValid(invoice))
                 {
                     sb.AppendLine(ErrorMessage);
                     continue;
                 }
-
+                
                 if (DateTime.Compare(invoice.IssueDate, invoice.DueDate) > 0)
                 {
                     sb.AppendLine(ErrorMessage);
                     continue;
                 }
 
+                //creating a valid invoice
                 Invoice invoiceToAdd = new Invoice
                 {
+                    // using identical properties in order to map successfully
                     Number = invoice.Number,
                     IssueDate = invoice.IssueDate,
                     DueDate = invoice.DueDate,
@@ -113,47 +133,58 @@
             }
 
             context.Invoices.AddRange(invoices);
+            
+            //actually importing info from data
             context.SaveChanges();
 
+            //using TrimEnd() to get rid of white spaces
             return sb.ToString().TrimEnd();
         }
 
         public static string ImportProducts(InvoicesContext context, string jsonString)
         {
+            //using Data Transfer Object Class to map it with products
             var productsArray = JsonConvert.DeserializeObject<ImportProductsDTO[]>(jsonString);
 
+            //using StringBuilder to gather all info in one string
             StringBuilder sb = new StringBuilder();
 
-
+            //creating List where all valid products can be kept
             List<Product> products = new List<Product>();
 
+            // taking only unique clients
             int[] uniqueClients = context.Clients
                 .Select(c => c.Id)
                 .ToArray();
 
             foreach (ImportProductsDTO product in productsArray)
             {
-                Product productToAdd = new Product
-                {
-                    Name = product.Name,
-                    Price = product.Price,
-                    CategoryType = product.CategoryType
-                };
-
+                //validating info for product from data
                 if (!IsValid(product))
                 {
                     sb.AppendLine(ErrorMessage);
                     continue;
                 }
 
+                //creating a valid product
+                Product productToAdd = new Product
+                {
+                    // using identical properties in order to map successfully
+                    Name = product.Name,
+                    Price = product.Price,
+                    CategoryType = product.CategoryType
+                };
+
                 foreach (int clientId in product.Clients.Distinct())
                 {
+                    //validating only unique clients
                     if (!uniqueClients.Contains(clientId))
                     {
                         sb.AppendLine(ErrorMessage);
                         continue;
                     }
 
+                    //adding valid clients
                     productToAdd.ProductsClients.Add(new ProductClient()
                     {
                         ClientId = clientId
@@ -166,6 +197,8 @@
             }
 
             context.Products.AddRange(products);
+
+            //actually importing info from data
             context.SaveChanges();
 
             return sb.ToString().TrimEnd();
